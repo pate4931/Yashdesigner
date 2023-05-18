33cf76dfb5fcaf4ad6844ee003e58b2af8f34d2c
Yash <pate4931@sheridancollege.ca>
2023-05-18 15:24:02 -0400

    my second commit

---
+++ src/softwaredesign/Softwaredesign.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package softwaredesign;
+
+/**
+ *
+ * @author     :YashKumar Patel 
+   @Student Id :991705950        
+ */
+public class Softwaredesign {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Yash is laerning ");
+          
+        // TODO code application logic here
+    }
+
+}
